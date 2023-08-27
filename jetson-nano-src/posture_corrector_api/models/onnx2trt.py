da0a82dc8167663b4cae3acd3feffc465df5273d
Rayan Meraghni <100576473+R40835@users.noreply.github.com>
2023-08-27 18:05:30 +0100

    Create onnx2trt.py
    the optimisation script to convert the ONNX model to a TensroRT engine

---
+++ jetson-nano-src/posture_corrector_api/models/onnx2trt.py
@@ -0,0 +1,30 @@
+import tensorrt as trt
+import pycuda.driver as cuda
+import pycuda.autoinit
+import numpy as np 
+
+onnx_path = 'model_th2.onnx'
+
+# set up TensorRT
+TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
+builder = trt.Builder(TRT_LOGGER)
+network = builder.create_network(1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH))
+parser = trt.OnnxParser(network, TRT_LOGGER) 
+
+with open(onnx_path, 'rb') as model:
+    parser.parse(model.read())
+    # model optimisations
+    config = builder.create_builder_config()
+    config.set_flag(trt.BuilderFlag.SPARSE_WEIGHTS)
+    config.set_flag(trt.BuilderFlag.FP16)
+    # enforcing fp16 types
+    config.set_flag(trt.BuilderFlag.OBEY_PRECISION_CONSTRAINTS)
+    # allocating max memory workspace
+    config.max_workspace_size = 1 << 30
+    # build and serialize engine
+    serialized_engine = builder.build_serialized_network(network, config)
+
+trt_path = 'movenet_v3.trt'
+
+with open(trt_path, 'wb') as f:
+    f.write(serialized_engine)
